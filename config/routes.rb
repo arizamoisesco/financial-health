d1bf0b3f6a520966a637120248a7f13d748adafa
Moises Ariza <arizamoises@gmail.com>
2022-07-24 12:54:23 -0500

    Sidebar customization, about message creation and connection of the basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :incomes
   resources :categories
   devise_for :users
+  get '/about', to:'about#about'
   root 'main#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
