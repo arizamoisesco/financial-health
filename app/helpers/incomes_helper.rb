cb5631c1be65bab207da9cce3fb4db3684ad4fe7
Moises Ariza <arizamoises@gmail.com>
2022-07-30 22:08:40 -0500

    Modify gemfile for deployment

--- app/helpers/incomes_helper.rb
+++ app/helpers/incomes_helper.rb
@@ -1,2 +1,10 @@
 module IncomesHelper
+    def graphic_incomes
+        descriptions = Income.group(:description).count.keys
+        moneys = Income.group(:value).count.keys
+        total_result = {}
+
+        descriptions.each_with_index {|key, value| total_result[key] = moneys[value]}
+
+    end
 end
