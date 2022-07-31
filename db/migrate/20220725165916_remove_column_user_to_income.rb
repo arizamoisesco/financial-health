005f76997d903b93862d5f64079d3c505c2650a9
Moises Ariza <arizamoises@gmail.com>
2022-07-31 16:59:43 -0500

    Delete migrations no necesary

--- db/migrate/20220725165916_remove_column_user_to_income.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnUserToIncome < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :incomes, :userId_id
-  end
-end
--- db/migrate/20220725173241_remove_column_user_to_expense.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnUserToExpense < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :expenses, :userId_id
-  end
-end
